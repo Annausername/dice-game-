b884faad50a90c870f40e5a884f414667cb47b35
Anna <annklimova10@gmail.com>
2022-09-08 19:28:57 +0000

    Add edit Player's name function and a function to generate random numbers

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,34 +2,34 @@
 
 // Edit player's name function
 function editName() {
-    
+    var newName = prompt("Enter Your Name");
+    document.querySelector('#player-name').innerHTML = newName;
 }
 
-function assignScore( ) 
+function assignScore() 
+
 
 function generateNumbers() {
-    
+    let num1 = Math.floor(Math.random() * 8) + 1;
+    let num2 = Math.floor(Math.random() * 8) + 1;
 
     // Add correspondent to a number generated image
-   
+    document.querySelector(".img1").setAttribute("src",
+    "assets/images/dice" + num1 + ".png");
+
+    document.querySelector(".img2").setAttribute("src",
+       "assets/images/dice" + num2 + ".png");
 
     //Accumulate scores for a Player (max 100)
-   
+    assignScore('player', num1);
     
     //Accumulate scores for the Computer (max 100)
-  
+    assignScore('computer', num2);
 
     // Pass each score '100' to Wins '+1' for Player
    
 
-    // Pass each score '100' to Wins '+1' for the Computer
-   
-}
-
 // Roll the dice
-function rollTheDice() {
-    // Generate random numbers
-   
-}
 
 // End game
+function endGame()
