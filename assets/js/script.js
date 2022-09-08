a925397ddb5bf6ace44b6718c6823bd3eb8797a7
Anna <annklimova10@gmail.com>
2022-09-08 19:25:45 +0000

    Add structure to JavaScript

---
+++ assets/js/script.js
@@ -0,0 +1,35 @@
+
+
+// Edit player's name function
+function editName() {
+    
+}
+
+function assignScore( ) 
+
+function generateNumbers() {
+    
+
+    // Add correspondent to a number generated image
+   
+
+    //Accumulate scores for a Player (max 100)
+   
+    
+    //Accumulate scores for the Computer (max 100)
+  
+
+    // Pass each score '100' to Wins '+1' for Player
+   
+
+    // Pass each score '100' to Wins '+1' for the Computer
+   
+}
+
+// Roll the dice
+function rollTheDice() {
+    // Generate random numbers
+   
+}
+
+// End game
